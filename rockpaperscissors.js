1b6bef9dd8dce913fc4a92e16cf9f392a16132fe
unknown <hoon81@gmail.com>
2024-03-19 20:06:11 -0400

    Add playRound function
    Function will take two parameters, play a round of the game and return
    the result.

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,9 +1,49 @@
+// Randomly select and return the computer's choice
 function getComputerChoice() {
     const choice = ["Rock", "Paper", "Scissors"];
     let temp = Math.floor((Math.random() * 3));
-    console.log(temp);
-    console.log(choice[temp]);
     return choice[temp];
 }
 
-getComputerChoice();
+// Play a round and return the result
+function playRound(playerSelection, computerSelection) {
+    const player = playerSelection.toUpperCase();
+    const computer = computerSelection.toUpperCase();
+    console.log(player);
+    console.log(computer);
+
+    let result = "";
+    if (player === "ROCK") {
+        if (computer === "ROCK") {
+            result = "Draw.";
+        } else if (computer === "PAPER") {
+            result = `You lose! ${computer} beats ${player}.`;
+        } else {
+            result = `You win! ${player} beats ${computer}.`;
+        }
+    } else if (player === "PAPER") {
+        if (computer === "ROCK") {
+            result = `You win! ${player} beats ${computer}.`;
+        } else if (computer === "PAPER") {
+            result = "Draw.";
+        } else {
+            result = `You lose! ${computer} beats ${player}.`;
+        }
+    } else if (player === "SCISSORS") {
+        if (computer === "ROCK") {
+            result = `You lose! ${computer} beats ${player}.`;
+        } else if (computer === "PAPER") {
+            result = `You win! ${player} beats ${computer}.`;
+        } else {
+            result = "Draw.";
+        }
+    } else {
+        result = "Invalid input.";
+    }
+    return result;
+}
+
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
