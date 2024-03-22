3480541b746df52d94cb00da741bc81226495ae8
unknown <hoon81@gmail.com>
2024-03-22 12:03:20 -0400

    Add function to determine winner
    Scores for player and computer are kept tracked and either one reaching
    5 points is declared to be a winner. Message will display and scores
    will reset, and a new game begins.

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -24,43 +24,93 @@ function playRound(playerSelection, computerSelection) {
             result = messages[0];
         } else if (computer === "PAPER") {
             result = messages[2];
+            scoreC++;
         } else { // (computer === SCISSORS)
             result = messages[1];
+            scoreP++;
         }
     } else if (player === "PAPER") {
         if (computer === "ROCK") {
             result = messages[1];
+            scoreP++;
         } else if (computer === "PAPER") {
             result = messages[0];
         } else { // (computer === SCISSORS)
             result = messages[2];
+            scoreC++;
         }
     } else if (player === "SCISSORS") {
         if (computer === "ROCK") {
             result = messages[2];
+            scoreC++;
         } else if (computer === "PAPER") {
             result = messages[1];
+            scoreP++;
         } else { // (computer === SCISSORS)
             result = messages[0];
         }
     } else {
         result = "Invalid input.";
     }
-
-    const display = document.querySelector(".display");
     display.textContent = result;
 }
+
 
 const btnR = document.querySelector(".rock")
 const btnP = document.querySelector(".paper")
 const btnS = document.querySelector(".scissors")
+const display = document.querySelector(".display");
+const display2 = document.querySelector(".display2");
+
+let scoreP = 0;
+let scoreC = 0;
+const playerWin = "** Your score is 5! You WIN! Resetting scores and new round begins.. **";
+const computerWin = "** Computer's score is 5! You LOSE! Resetting scores and new round begins.. **";
 
 btnR.addEventListener("click", () => {
-    playRound("ROCK", getComputerChoice())
+    playRound("ROCK", getComputerChoice());
+    console.log(scoreP, scoreC);
+    display2.textContent = "";
+    if (scoreP === 5) {
+        display2.textContent = playerWin;
+        scoreP = 0;
+        scoreC = 0;
+    };
+    if (scoreC === 5) {
+        display2.textContent = computerWin;
+        scoreP = 0;
+        scoreC = 0;
+    };
 });
+
 btnP.addEventListener("click", () => {
-    playRound("PAPER", getComputerChoice())
+    playRound("PAPER", getComputerChoice());
+    console.log(scoreP, scoreC);
+    display2.textContent = "";
+    if (scoreP === 5) {
+        display2.textContent = playerWin;
+        scoreP = 0;
+        scoreC = 0;
+    };
+    if (scoreC === 5) {
+        display2.textContent = computerWin;
+        scoreP = 0;
+        scoreC = 0;
+    };
 });
+
 btnS.addEventListener("click", () => {
-    playRound("SCISSORS", getComputerChoice())
+    playRound("SCISSORS", getComputerChoice());
+    console.log(scoreP, scoreC);
+    display2.textContent = "";
+    if (scoreP === 5) {
+        display2.textContent = playerWin;
+        scoreP = 0;
+        scoreC = 0;
+    };
+    if (scoreC === 5) {
+        display2.textContent = computerWin;
+        scoreP = 0;
+        scoreC = 0;
+    };
 });
